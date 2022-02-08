6aeedaee70159508dd2022b75558e73e4ae58513
can378 <yunji378mm@naver.com>
2022-02-09 04:20:51 +0900

    알바, 연구 내부 텍스트 채우기
    ^_^

--- Assets/Scripts/Lists/ResearchManager.cs
+++ Assets/Scripts/Lists/ResearchManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +7,18 @@ using UnityEngine.UI;
 public class ResearchManager : MonoBehaviour
 {
 
+    //추가 된 Prefab 수
+    static int Prefabcount=0;
 
+    //Research Info  적용할 것들
     public GameObject titleText;
+    public GameObject explanationText;
+    public GameObject coinNum;
+    public GameObject levelNum;
 
 
     //Research Info
-    //text파일로 저장한 후 TextAsset으로 읽어와도 되긶다ㅏ
+    //text파일로 저장한 후 TextAsset으로 읽어와도 되긴하다. 근데 일단 수가 적으니까
     string[] ResearchName = { "딸기 가치 상승", "딸기 성장기간 감소", "트럭 수익 상승", "비가 온다!", "잡초 감소", "벌레 감소", "미정" };
     string[] ResearchExplanation = { "딸기의 가치가 5%->10% 높아진다", " 딸기의 성장시간이 5%->10% 감소한다",
             "트럭으로 딸기를 판매할 때 5%->10% 높은 가격으로 판매한다.","소나기가 내릴 확률이 5%->10% 증가한다.",
@@ -24,8 +31,12 @@ public class ResearchManager : MonoBehaviour
 
     void Start()
     {
-
-        Debug.Log(titleText.GetComponent<Text>());
+        
+        titleText.GetComponent<Text>().text = ResearchName[Prefabcount];
+        explanationText.GetComponent<Text>().text = ResearchExplanation[Prefabcount];
+        coinNum.GetComponent<Text>().text = ResearchPrice[Prefabcount].ToString();
+        levelNum.GetComponent<Text>().text = ResearchLevel[Prefabcount].ToString();
+        Prefabcount++;
 
     }
 
