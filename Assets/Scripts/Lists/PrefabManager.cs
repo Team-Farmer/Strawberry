9733b93e8e98db7e4995ab7047a3b175efd876b5
can378 <yunji378mm@naver.com>
2022-02-16 22:57:04 +0900

    3명만 고용가능하게
    왜 안되는 건지 모르겠음.

--- Assets/Scripts/Lists/PrefabManager.cs
+++ Assets/Scripts/Lists/PrefabManager.cs
@@ -11,15 +11,13 @@ public class PrefabManager : MonoBehaviour
     {
         public string Name,Explanation;
         public int Price, Level;
-        public bool employ;
 
-        public PrefabStruct(string Name,string Explanation, int Price, int Level,bool employ)
+        public PrefabStruct(string Name,string Explanation, int Price, int Level)
         {
             this.Name = Name;
             this.Explanation = Explanation;
             this.Price = Price;
             this.Level = Level;//PTJ이라면 고용여부 의미. 0이 고용안함 1이 고용함
-            this.employ=employ;
         }
     }
 
@@ -41,9 +39,14 @@ public class PrefabManager : MonoBehaviour
     int prefabnum;
 
 
+    //몇명 고용중인지 확인
+    private static int employCount;
+
+
     //===================================================================================================
     void Start()
     {
+       
         InfoUpdate();
     }
     void Update()
@@ -55,7 +58,7 @@ public class PrefabManager : MonoBehaviour
     
     
     //coin 버튼 -> 연구 레벨, 코인 변화
-    public void clickCoin() {
+    public void clickCoin_Research() {
 
         //레벨이 올라간다.
         Info[prefabnum].Level++;
@@ -64,7 +67,7 @@ public class PrefabManager : MonoBehaviour
         //해당 금액의 코인이 감소된다.
         GameManager.instance.coin -= Info[prefabnum].Price;
         GameManager.instance.ShowCoinText(GameManager.instance.coin);
-        //gm1.CoinText.text = gm1.coin.ToString() + " A";
+        
 
     }
 
@@ -72,44 +75,55 @@ public class PrefabManager : MonoBehaviour
     //coin 버튼 -> 알바 레벨, 고용 여부
     public void clickCoin_PTJ() 
     {
-
-        if (Info[prefabnum].Level == 0) //고용 중이 아니라면 !!!!!!!!!!!!왜 prefabnum말로 0을 넣어도 되는가
+        if (employCount >= 3) { Debug.Log("3명이 넘게 고용하지 못합니다."); }
+        else
         {
-            //해당 금액의 코인이 감소된다.
-            GameManager.instance.coin -= Info[prefabnum].Price;
-            GameManager.instance.ShowCoinText(GameManager.instance.coin);
-            //gm1.CoinText.text = gm1.coin.ToString() + " A";
+            if (Info[prefabnum].Level == 0) //고용 중아니고 3명이하 일하고 있으면 고용가능            //의문점 = 왜 prefabnum말로 0을 넣어도 되는가
+            {
+                //해당 금액의 코인이 감소된다.
+                GameManager.instance.coin -= Info[prefabnum].Price;
+                GameManager.instance.ShowCoinText(GameManager.instance.coin);
 
-            Info[prefabnum].Level = 1;
-            levelNum.GetComponent<Text>().text = "고용중";
+                employCount += 1;
+                Info[prefabnum].Level = 1;//1=고용
+                levelNum.GetComponent<Text>().text = "고용";
 
+            }
+            else //고용중이라면 무직으로 변경
+            {
+                employCount -= 1;
+                Info[prefabnum].Level = 0;//0=무직
+                levelNum.GetComponent<Text>().text = "무직";
+
+            }
         }
-        else //고용중이라면
-        {
-            Info[prefabnum].Level = 0;
-            levelNum.GetComponent<Text>().text = "무직";
-        }
+        //Debug.Log("count="+employCount);
+        //Debug.Log("employ=" + Info[prefabnum].Level);
 
     }
 
 
     public void InfoUpdate()
     {
-        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!주의!!!!!!!!!!!!!7 저 숫자는 프리팹 숫자와 관련되어 있다!!! 같이 조절해야함
+        //!!!!!!!!!!!!!!주의!!!!!!!!!!!!!숫자 7은 프리팹 숫자와 관련되어 있다!!! 같이 조절해야함
+        
+        //프리팹들에게 고유 번호 붙이기
         if (Prefabcount >= 7)
         { Prefabcount -= 7; }
         prefabnum = Prefabcount;
 
+
+        //타이틀, 설명, 코인값, 레벨, 고용여부 텍스트에 표시
         titleText.GetComponent<Text>().text = Info[Prefabcount].Name;
         explanationText.GetComponent<Text>().text = Info[Prefabcount].Explanation;
         coinNum.GetComponent<Text>().text = Info[Prefabcount].Price.ToString();
-
         if (Info[Prefabcount].Level == 0)//level이 0이라면 PTJ과 관련된 것이다.
-        {    levelNum.GetComponent<Text>().text = "무직";    }
+        {    levelNum.GetComponent<Text>().text = " ";    }
         else
         {    levelNum.GetComponent<Text>().text = Info[Prefabcount].Level.ToString();    }
 
-        //Debug.Log(prefabnum);
+
+        
         Prefabcount++;
     }
 }
