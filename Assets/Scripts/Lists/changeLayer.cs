40318b382593d2872f29dc28592a319fb9821113
can378 <yunji378mm@naver.com>
2022-03-29 01:24:34 +0900

    베리
    베리 프리팹 단일화
    스트립트 간소화
    등..

--- Assets/Scripts/Lists/changeLayer.cs
+++ Assets/Scripts/Lists/changeLayer.cs
@@ -4,71 +4,68 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 
-public class changeLayer : MonoBehaviour
+public class ChangeLayer : MonoBehaviour
 {
-	public bool isChallengeNews;
-    [Header("====Buttons====")]
-    [SerializeField]
-    private GameObject[] tagButtons;
-    public Sprite[] tagButtons_Image;
-    public Sprite[] tagButtons_selectImage;
 
-    [Header("====ScrollView====")]
-    //베리 오브젝트 부모 오브젝트
-    [SerializeField]
-    private GameObject content1;
-    [SerializeField]
-    private GameObject content2;
+	[SerializeField]
+	private bool isBerry;
+	[Header("====Buttons====")]
+	[SerializeField]
+	private GameObject[] tagButtons;//버튼 gameObject
+	public Sprite[] tagButtons_Image;//눌리지 않은 버튼 이미지
+	public Sprite[] tagButtons_selectImage;//눌린 버튼 이미지
 
-
-    private GameObject[] target_berry;
-
-    [Header("========Swipe List=======")]
-    [SerializeField]
-    private Scrollbar scrollBar;                    // Scrollbar의 위치를 바탕으로 현재 페이지 검사
-    [SerializeField]
-    private float swipeTime = 0.1f;         // 페이지가 Swipe 되는 시간
-    [SerializeField]
-    private float swipeDistance = 1.0f;        // 페이지가 Swipe되기 위해 움직여야 하는 최소 거리
+	[Header("====ScrollView====")]
+	//베리 content들 겸 Challenge News 오브젝트
+	[SerializeField]
+	private GameObject[] content;
 
 
+	[Header("========Swipe=======")]
+	[SerializeField]
+	private Scrollbar scrollBar;                    // Scrollbar의 위치를 바탕으로 현재 페이지 검사
+	[SerializeField]
+	private float swipeTime = 0.1f;         // 페이지가 Swipe 되는 시간
+	[SerializeField]
+	private float swipeDistance = 1.0f;        // 페이지가 Swipe되기 위해 움직여야 하는 최소 거리
 
-    private float[] scrollPageValues;           // 각 페이지의 위치 값 [0.0 - 1.0]
-    private float valueDistance = 0;            // 각 페이지 사이의 거리
-    private int currentPage = 0;            // 현재 페이지
-    private int maxPage = 2;                // 최대 페이지 2로 설정
 
-    private float startTouchX;              // 터치 시작 위치
-    private float endTouchX;                    // 터치 종료 위치
+	//============================================================================
+	//SWIPE 변수들
 
-    private float startScroll;
-    private float endScroll;
+	private float[] scrollPageValues;           // 각 페이지의 위치 값 [0.0 - 1.0]
+	private float valueDistance = 0;            // 각 페이지 사이의 거리
+	private int currentPage = 0;            // 현재 페이지
+	private int maxPage = 4;                // 최대 페이지 2로 설정
 
-    private bool isSwipeMode = false;       // 현재 Swipe가 되고 있는지 체크
+	private float startTouchX;              // 터치 시작 위치
+	private float endTouchX;                    // 터치 종료 위치
+
+	private bool isSwipeMode = false;       // 현재 Swipe가 되고 있는지 체크
 
 
 	//===========================================================================================================
-    private void Awake()
-    {
-        // 스크롤 되는 페이지의 각 value 값을 저장하는 배열 메모리 할당
-        scrollPageValues = new float[2];
+	private void Awake()
+	{
+		if (isBerry == true)
+		{
+			// 스크롤 되는 페이지의 각 value 값을 저장하는 배열 메모리 할당
+			scrollPageValues = new float[4];
 
-        // 스크롤 되는 페이지 사이의 거리
-        valueDistance = 1f / (scrollPageValues.Length - 1f);
+			// 스크롤 되는 페이지 사이의 거리
+			valueDistance = 1f / (scrollPageValues.Length - 1f);
 
-        // 스크롤 되는 페이지의 각 value 위치 설정 [0 <= value <= 1]
-        for (int i = 0; i < scrollPageValues.Length; ++i)
-        {
-            scrollPageValues[i] = valueDistance * i;
-        }
-
-    }
-    void Start()
-    {
-		if (isChallengeNews == true)
-		{ }
-        else 
-		{ 
+			// 스크롤 되는 페이지의 각 value 위치 설정 [0 <= value <= 1]
+			for (int i = 0; i < scrollPageValues.Length; ++i)
+			{
+				scrollPageValues[i] = valueDistance * i;
+			}
+		}
+	}
+	void Start()
+	{
+		if (isBerry == true)
+		{
 			//처음에는 berry classic
 			selectBerryTag("berry_classic");
 
@@ -76,54 +73,23 @@ public class changeLayer : MonoBehaviour
 			SetScrollBarValue(0);
 		}
 
-    }
+	}
 
 	private void Update()
 	{
-		if (isChallengeNews == false)
-		{ UpdateInput(); }
-
+		if(isBerry ==true)	UpdateInput();
 	}
 
 	//===========================================================================================================
-	public void SetScrollBarValue(int index)
-	{
-		currentPage = index;
-		scrollBar.value = scrollPageValues[index];
-	}
 
-	public void swipeButton(int value)
-	{
-		switch (value)
-		{
+    #region SWIPE
 
-			case 0:
-				// 현재 페이지가 왼쪽 끝이면 종료
-				if (currentPage == 0) return;
-				// 왼쪽으로 이동. 현재 페이지를 1 감소
-				currentPage--;
-				SetScrollBarValue(0);
-
-				break;
-
-			case 1:
-				// 현재 페이지가 오른쪽 끝이면 종료
-				if (currentPage == maxPage - 1) return;
-				// 오른쪽으로 이동. 현재 페이지를 1 증가
-				currentPage++;
-				SetScrollBarValue(1);
-				break;
-
-		}
-
-	}
-
-	private void UpdateInput()
+    private void UpdateInput()
 	{
 		// 현재 Swipe를 진행중이면 터치 불가
 		if (isSwipeMode == true) return;
 
-		#if UNITY_EDITOR
+#if UNITY_EDITOR
 		// 마우스 왼쪽 버튼을 눌렀을 때 1회
 		if (Input.GetMouseButtonDown(0))
 		{
@@ -139,9 +105,9 @@ public class changeLayer : MonoBehaviour
 
 			UpdateSwipe();
 		}
-		#endif
+#endif
 
-		#if UNITY_ANDROID
+#if UNITY_ANDROID
 		if (Input.touchCount == 1)
 		{
 			Touch touch = Input.GetTouch(0);
@@ -161,7 +127,7 @@ public class changeLayer : MonoBehaviour
 				UpdateSwipe();
 			}
 		}
-		#endif
+#endif
 
 
 	}
@@ -230,80 +196,137 @@ public class changeLayer : MonoBehaviour
 		isSwipeMode = false;
 	}
 
-	public void TagImageChange(int index) {
+	#endregion
 
-        //버튼 스프라이트 다 안눌린거로
-        for (int i = 0; i < tagButtons_Image.Length; i++) {
-            tagButtons[i].GetComponent<Image>().sprite = tagButtons_Image[i];
-        }
+	//스트롤바 위치 변경
+	public void SetScrollBarValue(int index)
+	{
+		currentPage = index;
+		scrollBar.value = scrollPageValues[index];
+	}
 
-        //해당 버튼 스프라이트만 눌린거로
-        tagButtons[index].GetComponent<Image>().sprite = tagButtons_selectImage[index];
-    
-    }
-	public void TurnOn(GameObject obj) { content1.SetActive(false);content2.SetActive(false); obj.SetActive(true); }
+	public void swipeButton(string value)
+	{
+		switch (value)
+		{
 
-    //버튼을 눌렀을 때 해당 분류의 딸기를 보인다.name=tag이름
-    public void selectBerryTag(string name)
-    {
+			case "left":
+				// 현재 페이지가 왼쪽 끝이면 종료
+				if (currentPage == 0) return;
 
-        //선택 베리들 보이게 활성화
-        Active(name);
-        
-        //다른 베리들 안보이게 비활성화
-        switch (name) {
-            case "berry_classic": inActive("berry_special"); inActive("berry_unique"); break;
-            case "berry_special": inActive("berry_classic"); inActive("berry_unique"); break;
-            case "berry_unique": inActive("berry_special"); inActive("berry_classic"); break;
-        }
+				currentPage--;// 왼쪽으로 이동 = 현재 페이지 1 감소
+				SetScrollBarValue(currentPage);
+
+				break;
+
+			case "right":
+				// 현재 페이지가 오른쪽 끝이면 종료
+				if (currentPage == maxPage - 1) return;
+
+				// 오른쪽으로 이동 = 현재 페이지 1 증가
+				currentPage++;
+				SetScrollBarValue(currentPage);
+
+				break;
+
+		}
+
+	}
 
 
-		//horizontal scrollbar 처음으로 돌리기
-		//scrollBar.transform.GetComponent<Scrollbar>().value = 0;
+
+
+
+
+
+	//버튼 누르는 효과
+	public void TagImageChange(int index)
+	{
+
+		//버튼 스프라이트 다 안눌린거로
+		for (int i = 0; i < tagButtons_Image.Length; i++)
+		{
+			tagButtons[i].GetComponent<Image>().sprite = tagButtons_Image[i];
+		}
+
+		//해당 버튼 스프라이트만 눌린거로
+		tagButtons[index].GetComponent<Image>().sprite = tagButtons_selectImage[index];
+
+	}
+
+	//challenge News 창 변경
+	public void TurnOn(GameObject obj) 
+	{
+		for (int i = 0; i < content.Length; i++) { content[i].SetActive(false); } //challenge, news 둘 다 비활성화
+		obj.SetActive(true); //해당 창만 활성화
+	}
+
+
+
+
+	//버튼을 눌렀을 때
+	//해당 분류의 딸기를 보인다
+	public void selectBerryTag(string name)
+	{
+
+		//모든 베리들 보이게 활성화
+		AllActive();
+
+		//다른 베리들 안보이게 비활성화
+		switch (name)
+		{
+			case "berry_classic": inActive("berry_special"); inActive("berry_unique"); break;
+			case "berry_special": inActive("berry_classic"); inActive("berry_unique"); break;
+			case "berry_unique": inActive("berry_special"); inActive("berry_classic"); break;
+		}
+
+
 		SetScrollBarValue(0);
 
 	}
 
-    public void inActive(string name) {
-
-        //해당 태그를 가진 오브젝트를 찾는다.
-        target_berry = GameObject.FindGameObjectsWithTag(name);
-
-        //그 오브젝트를 비활성화한다.
-        for (int i = 0; i < target_berry.Length; i++)
-        {
-            target_berry[i].SetActive(false);
-        }
-
-    }
-
-    public void Active(string name)
-    {
-        //모든 베리 오브젝트 활성화
-        int iCount = content1.transform.childCount;
-        for (int i = 0; i < iCount; i++)
-        {
-            Transform trChild = content1.transform.GetChild(i);
-            trChild.gameObject.SetActive(true);
-        }
-        int iCount2 = content2.transform.childCount;
-        for (int i = 0; i < iCount2; i++)
-        {
-            Transform trChild2 = content2.transform.GetChild(i);
-            trChild2.gameObject.SetActive(true);
-        }
 
 
-        //해당 베리들 보이게 활성화
-        target_berry = GameObject.FindGameObjectsWithTag(name);
 
-        //그 오브젝트를 활성화한다.
-        for (int i = 0; i < target_berry.Length; i++)
-        {
-            target_berry[i].SetActive(true);
-        }
 
-    }
+	public void inActive(string name)
+	{
+		int index = 0;
+		switch (name)
+		{
+			case "berry_classic": index = 0; break;
+			case "berry_special": index = 16; break;
+			case "berry_unique": index = 32; break;
+		}
+
+		//비활성화
+		for (int j = 0; j < 4; j++)
+		{
+			for (int i = index; i < index + 16; i++)
+			{
+				Transform trChild = content[j].transform.GetChild(i);
+				trChild.gameObject.SetActive(false);
+			}
+		}
+		
+
+
+	}
+
+
+	public void AllActive()
+	{
+		//모든 베리 오브젝트 활성화
+		for (int j = 0; j < 4; j++)
+		{
+			for (int i = 0; i < content[j].transform.childCount; i++)
+			{
+				Transform trChild = content[j].transform.GetChild(i);
+				trChild.gameObject.SetActive(true);
+			}
+
+		}
+	}
 
 
 }
