1d3569fbdd63d10df280b68b35d6e94ee35f5a99
wjawksl <wjawksl@gmail.com>
2022-03-31 00:35:54 +0900

    03.31

--- Assets/Scripts/BerryField/RainCtrl.cs
+++ Assets/Scripts/BerryField/RainCtrl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RainCtrl : MonoBehaviour
 {
@@ -8,25 +9,34 @@ public class RainCtrl : MonoBehaviour
     public float rainPeriod;
     public int mult;
     public float rainTime;
+    
     void Awake()
     {
-        rainParticle = GetComponent<ParticleSystem>();
+        rainParticle = GetComponent<ParticleSystem>();       
     }
     void Update()
     {
         rainTime += Time.deltaTime;
         if(rainTime >= rainPeriod)
         {
-            mult = 3;
-            rainTime = 0;
-            rainParticle.Play();
-
-            StartCoroutine(RainRoutine());
+            Raining();
+            StartCoroutine(RainingRoutine());
         }
     }
-    IEnumerator RainRoutine()
+    void Raining()
     {
-        yield return new WaitForSeconds(5f);
+        mult = 3;
+        rainTime = 0;
+        rainParticle.Play();
+        
+        Color bgColor = GameObject.FindGameObjectWithTag("BG").GetComponent<Image>().color;
+        bgColor.a = 200/255f;
+        Debug.Log(bgColor.a);
+        GameObject.FindGameObjectWithTag("BG").GetComponent<Image>().color = bgColor;
+    }
+    IEnumerator RainingRoutine()
+    {        
+        yield return new WaitForSeconds(rainParticle.main.duration);
         mult = 1;
     }
 }
