e588e77807f72440e532add24feab7ab0da9eb16
YerimJung <60317213+YerimJung@users.noreply.github.com>
2022-05-13 16:49:21 +0900

    타이머수정

--- Assets/Scripts/Store/MiniGame1.cs
+++ Assets/Scripts/Store/MiniGame1.cs
@@ -6,30 +6,58 @@ using UnityEngine.UI;
 public class MiniGame1 : MonoBehaviour
 {
     public Scrollbar scrollbar;
+    public Text count_txt;
     float size;
-    int time = 60;
+    int time = 63;
 
     void OnEnable()
     {
         size = scrollbar.size/60f;
 
-        //3초 카운트
+        StartGame();
+    }
 
-        //게임시작
-        StartCoroutine(Timer());
+    void StartGame()
+    {
+        //3초 카운트
+        count_txt.gameObject.SetActive(true);
+        StartCoroutine(Count3Second());
+    }
+
+    IEnumerator Count3Second()
+    {
+        count_txt.text = (time - 60).ToString();
+        yield return new WaitForSeconds(1);
+        time -= 1;
+        if (time <= 60)
+        {
+            count_txt.gameObject.SetActive(false);
+            StartCoroutine(Timer());
+        }
+        else
+        {
+            StartCoroutine(Count3Second());
+        }
     }
 
     IEnumerator Timer()
     {
+        yield return new WaitForSeconds(1);
         scrollbar.size -= size;
         time -= 1;
-        yield return new WaitForSeconds(1);
-        if (time == 0) StopGame();
-        else StartCoroutine(Timer());
+        if (time == 0)
+        {
+            StopGame();
+        }
+        else
+        {
+            StartCoroutine(Timer());
+        }
     }
 
     void StopGame()
     {
         Debug.Log("게임 끝");
+        time = 63;
     }
 }
