5718b0bb077eddd16c17500e6dd7f4a4b0ebfcc9
Siwon <fhrhdls@naver.com>
2022-05-19 14:41:57 +0900

    인트로 배경 색 변경

--- Assets/Scripts/UI/Main.cs
+++ Assets/Scripts/UI/Main.cs
@@ -6,18 +6,27 @@ using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
+    public GameObject ad;
     // Use this for initialization
     void Start()
     {
         gameObject.GetComponent<Image>().DOFade(0.0f, 3.5f);
         Invoke("ActiveOff", 3.0f);
         GameManager.instance.EnableObjColliderAll();
+        Invoke("ActiveOn", 3.0f);
+
+
     }
 
 
     public void ActiveOff()
     {
         gameObject.SetActive(false);
+    }
+
+    public void ActiveOn()
+    {
+        ad.SetActive(false);
     }
 
 }
