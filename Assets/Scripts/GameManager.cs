548ee7154629665e092c365de753410d5b60554c
can378 <yunji378mm@naver.com>
2022-05-20 15:58:54 +0900

    /
    저장이 안되는디
    혹시 내가 instance화 하나 해서그런가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -123,6 +123,7 @@ public class GameManager : MonoBehaviour
         //TimerStart += Instance_TimerStart;
 
         DisableObjColliderAll();       
+        
         PTJList.SetActive(true);
 
         //NEW BERRY
