13a1290b770816731a76247fe0c864e8231970f1
wooyn730 <80298207+wooyn730@users.noreply.github.com>
2022-05-21 22:54:56 +0900

    Revert "Merge branch 'main' of https://github.com/NearBySiwon/Strawberry"
    This reverts commit af02165a02f1b55a2a134ca52587003231d16945, reversing
    changes made to 44c1412e5af81481230fa02aa1d137d984d15106.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -487,17 +487,13 @@ public class GameManager : MonoBehaviour
         //¾û¸ÁÁøÃ¢ÄÚµå
         for (int i = 0; i < 3; i++)
         {
-            try
+            if (working[i].activeSelf == true)
             {
-                if (working[i].activeSelf == true)
-                {
-                    working[i].transform.GetChild(1).transform.GetComponent<Text>().text
-                        = DataController.instance.gameData.PTJNum[workingID[i]].ToString();
-                    if (DataController.instance.gameData.PTJNum[workingID[i]] == 0)
-                    { working[i].SetActive(false); }
-                }
+                working[i].transform.GetChild(1).transform.GetComponent<Text>().text
+                    = DataController.instance.gameData.PTJNum[workingID[i]].ToString();
+                if (DataController.instance.gameData.PTJNum[workingID[i]] == 0)
+                { working[i].SetActive(false); }
             }
-            catch { }
         }
     }
 
