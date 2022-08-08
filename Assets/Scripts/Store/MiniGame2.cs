52c763fb1408940230888fa6471d489fdcdbebac
wjawksl <89779753+wjawksl@users.noreply.github.com>
2022-08-09 00:40:14 +0900

    2022.08.09
    - 미니게임 2 초안

---
+++ Assets/Scripts/Store/MiniGame2.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniGame2 : MiniGame
+{
+    [Header("MiniGame1")]
+    public Image quiz_img;     //퀴즈딸기 이미지
+    public int quizIndex;      //퀴즈딸기 인덱스
+    public int[] answerIndex;  //정답딸기 인덱스(4개)
+    public Button[] answer_btn;//정답딸기 버튼(4개)
+    public Image[] answer_img; //정답딸기 이미지(4개)
+    public GameObject O;       //O 이미지
+    public GameObject X;       //X 이미지
+
+    protected override void Awake()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            int _i = i;
+            answer_btn[_i].onClick.AddListener(() => OnClickAnswerButton(_i));
+        }
+        answerIndex = new int[4];
+        base.Awake();
+    }
+
+    protected override void MakeGame()
+    {
+        quiz_img.gameObject.SetActive(true);
+        for (int i = 0; i < 4; i++)
+        {
+            answer_img[i].gameObject.SetActive(true);
+            answer_btn[i].enabled = true;
+        }
+
+        //퀴즈딸기 만들고 이미지 배치
+        quizIndex = unlockList[UnityEngine.Random.Range(0, unlockList.Count)];
+        quiz_img.sprite = global.berryListAll[quizIndex].GetComponent<SpriteRenderer>().sprite;
+
+        //랜덤의 정답딸기 인덱스(0~4)에 퀴즈딸기 배치
+        int randomAnswerIndex = UnityEngine.Random.Range(0, 4);
+        for (int i = 0; i < 4; i++)
+        {
+            if (randomAnswerIndex == i)
+            {
+                answer_img[i].sprite = quiz_img.sprite;
+                answerIndex[i] = quizIndex;
+            }
+            else
+            {
+                //정답인덱스나 다른 정답딸기들이랑 다른 딸기번호 나올때까지 랜덤번호로 뽑아서 정답딸기에 배치
+                answerIndex[i] = unlockList[UnityEngine.Random.Range(0, unlockList.Count)];
+                while (CheckIndex(i))
+                {
+                    answerIndex[i] = unlockList[UnityEngine.Random.Range(0, unlockList.Count)] ;
+                }
+                answer_img[i].sprite = global.berryListAll[answerIndex[i]].GetComponent<SpriteRenderer>().sprite;
+            }
+        }
+
+        bool CheckIndex(int idx)
+        {
+            if (answerIndex[idx] == quizIndex) return true;
+            for (int i = 0; i < 4; i++)
+            {
+                if (i == idx) continue;
+                if (answerIndex[idx] == answerIndex[i]) return true;
+            }
+            return false;
+        }
+    }
+
+    public void OnClickAnswerButton(int index)
+    {
+        //정답 : 10점 추가, 다음문제 출제
+        if (answerIndex[index] == quizIndex)
+        {
+            O.SetActive(true);
+            score += 10;
+            score_txt.text = score.ToString() + "점";
+        }
+        //오답 : 10초 줄기
+        else
+        {
+            X.SetActive(true);
+            scrollbar.size -= size * 10;
+            time -= 10;
+        }
+        if (time > 0)
+        {
+            Invoke("MakeNextQuiz", 0.3f);
+        }
+    }
+
+    void MakeNextQuiz()
+    {
+        O.SetActive(false);
+        X.SetActive(false);
+        MakeGame();
+    }
+
+    public override void StopGame()
+    {
+        base.StopGame();
+        //딸기 안보이게
+        quiz_img.gameObject.SetActive(false);
+        for (int i = 0; i < 4; i++)
+        {
+            answer_img[i].gameObject.SetActive(false);
+            answer_btn[i].enabled = false;
+        }
+        O.SetActive(false);
+        X.SetActive(false);
+    }
+
+    protected override void FinishGame()
+    {
+        base.FinishGame();
+
+        //최고기록 저장
+        if (DataController.instance.gameData.highScore[0] < score)
+        {
+            DataController.instance.gameData.highScore[0] = score;
+        }
+
+        //결과패널
+        resultPanel.SetActive(true);
+        result_txt.text = "최고기록 : " + DataController.instance.gameData.highScore[0] + "\n현재점수 : " + score;
+
+        
+
+        // 미니게임 1 보상 하트 공식(미니게임 1은 해금 하트가 60이다)
+        float gain_coin = score * research_level_avg * ((100 + 60 * 2) / 100f);
+
+        Debug.Log("얻은 코인:" + Convert.ToInt32(gain_coin));
+
+        //코인지급
+        GameManager.instance.GetCoin(Convert.ToInt32(gain_coin));
+
+        StopGame();
+    }
+}
--- Assets/Scripts/Store/MiniGameManager.cs
+++ Assets/Scripts/Store/MiniGameManager.cs
@@ -64,6 +64,9 @@ public class MiniGameManager : MonoBehaviour
             case 1:
                 infoText.text = "딸기 창고가 정전이 되었어요! 빨리 딸기들을 분류해서 팔아야 하는데... 안은 깜깜하고 어둑어둑한 바깥은 비까지 내려요. 하지만 우리는 하루 이틀 딸기를 수확한 게 아니죠! 어둠 속에서 최대한 많은 딸기들을 분류해 봐요!";
                 break;
+            case 2:
+                infoText.text = "잘 익은 딸기들 따서 모아뒀었는데, 날이 더워서인지 몇몇 딸기들이 살짝 무른 것 같아요. 하지만 우리에겐 굳은 신념이 있죠. 가장 달콤하고 가장 싱싱한 딸기들을 팔자! 그러니 눈을 크게 뜨고 상한 딸기들을 골라내봐요!";
+                break;
             case 3:
                 infoText.text = "딸기 밭에 폭풍이 지나갔어요! 밭에 열려있던 딸기들도, 수확해둔 딸기들도 모두 모두 날아가 버렸어요... 어라? 근데 날아갔던 딸기들이 하늘에서 떨어지네요?보고만 있을 때가 아니죠! 바구니로 멀쩡한 딸기들만 최대한 담아봐요!";
                 break;
@@ -80,10 +83,14 @@ public class MiniGameManager : MonoBehaviour
         {
             miniGameList[0].gameObject.SetActive(true);
         }
+        else if (info_str == "잘 익은 ")
+        {
+            miniGameList[1].gameObject.SetActive(true);
+        }
         else if(info_str == "딸기 밭에")
         {
             miniGameList[2].gameObject.SetActive(true);
-        }
+        }  
         else if (info_str == "아이고 딸")
         {
             miniGameList[3].gameObject.SetActive(true);
