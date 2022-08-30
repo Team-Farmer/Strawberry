9cd196f640fbef88a321335624fb552359fdec8e
wooyeon <bethelight730@gmail.com>
2022-08-30 18:28:26 +0900

    .

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -658,7 +658,7 @@ public class GameManager : MonoBehaviour
         //�� ���� ����======
 
         //PRICE
-        price_newBerry = 80 + 20 * (BerryCount("classic", true) + BerryCount("special", true) + BerryCount("unique", true));
+        price_newBerry = 90 + 10 * (BerryCount("classic", true) + BerryCount("special", true) + BerryCount("unique", true));
         //priceText_newBerry.GetComponent<Text>().text = price_newBerry.ToString();
         ShowCoinText(priceText_newBerry.GetComponent<Text>(), price_newBerry);
 
