d6ac1d71dde3152d24d0a76a387e422da4691720
시원 <96858320+NearthYou@users.noreply.github.com>
2022-09-11 16:44:37 +0900

    패널 애니메이션 수정

--- Assets/Scripts/UI/PanelAnimation.cs
+++ Assets/Scripts/UI/PanelAnimation.cs
@@ -15,31 +15,25 @@ public class PanelAnimation : MonoBehaviour
 
     public void OpenUP()
     {
-        RectTransform rt = GetComponent<RectTransform>();
         gameObject.SetActive(true);
-
-        rt.DOAnchorPosY(UpPosition, 0.3f);
+        gameObject.GetComponent<RectTransform>().DOAnchorPosY(UpPosition, 0.3f);
     }
 
     public void CloseUp()
     {
-        RectTransform rt = GetComponent<RectTransform>();
-        rt.DOAnchorPosY(-1940, 0.2f);
+        gameObject.GetComponent<RectTransform>().DOAnchorPosY(-1940, 0.2f);
         Invoke("ActiveOff", 0.3f);
     }
 
     public void OpenDown()
     {
-        RectTransform rt = GetComponent<RectTransform>();
         gameObject.SetActive(true);
-
-        rt.DOAnchorPosY(DownPosition, 0.3f);
+        gameObject.GetComponent<RectTransform>().DOAnchorPosY(DownPosition, 0.3f);
     }
 
     public void CloseDown()
     {
-        RectTransform rt = GetComponent<RectTransform>();
-        rt.DOAnchorPosY(1870, 0.3f);
+        gameObject.GetComponent<RectTransform>().DOAnchorPosY(1870, 0.3f);
         Invoke("ActiveOff", 0.3f);
     }
 
@@ -61,15 +55,14 @@ public class PanelAnimation : MonoBehaviour
 
     public void OpenScale()
     {
-        RectTransform rt = GetComponent<RectTransform>();
         gameObject.SetActive(true);
-        rt.transform.DOScale(new Vector3(1f, 1f, 1f), 0.3f);
+        gameObject.transform.DOScale(Vector3.zero, 0f);
+        gameObject.transform.DOScale(Vector3.one, 0.3f);
     }
 
     public void CloseScale()
     {
-        RectTransform rt = GetComponent<RectTransform>();
-        rt.transform.DOScale(new Vector3(0f, 0f, 1f), 0.3f);
+        gameObject.transform.DOScale(Vector3.zero, 0.3f);
         Invoke("ActiveOff", 0.2f);
     }
 
