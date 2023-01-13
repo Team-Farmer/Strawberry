4b973c131cff1cea427e9fbe66ceb7e48a58c70e
wooyn730 <80298207+wooyn730@users.noreply.github.com>
2023-01-13 13:58:43 +0900

    딸기 가격 다시 조금 줄임

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -827,7 +827,7 @@ public class GameManager : MonoBehaviour
         //새 딸기 개발======
 
         //PRICE
-        price_newBerry = 90 + 10 * (BerryCount("classic", true) + BerryCount("special", true) * 3 + BerryCount("unique", true) * 5);
+        price_newBerry = 90 + 10 * (BerryCount("classic", true) + BerryCount("special", true) * 2 + BerryCount("unique", true) * 4);
 
         ShowCoinText(priceText_newBerry.GetComponent<Text>(), price_newBerry);
 
